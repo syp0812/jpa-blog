0b37b63c7ddf1da6a200e690c8cea63501f83812
syp0812 <94878158+syp0812@users.noreply.github.com>
2022-12-12 22:30:03 +0900

    #2 댓글 좋아요 기능 구현

--- src/main/java/jpacrud/blog/dto/CommentRequestDto.java
+++ src/main/java/jpacrud/blog/dto/CommentRequestDto.java
@@ -1,10 +1,6 @@
 package jpacrud.blog.dto;
 
-import jpacrud.blog.entity.Board;
-import jpacrud.blog.entity.Member;
 import lombok.Getter;
-
-import java.util.List;
 
 @Getter
 public class CommentRequestDto {
---
+++ src/main/java/jpacrud/blog/service/CommentLikeService.java
@@ -0,0 +1,42 @@
+package jpacrud.blog.service;
+
+import jpacrud.blog.entity.Comment;
+import jpacrud.blog.entity.CommentLike;
+import jpacrud.blog.entity.Member;
+import jpacrud.blog.exception.BlogException;
+import jpacrud.blog.exception.BlogExceptionType;
+import jpacrud.blog.repository.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CommentLikeService {
+    private final MemberRepository memberRepository;
+    private final CommentRepository commentRepository;
+    private final CommentLikeRepository commentLikeRepository;
+
+    @Transactional
+    public void saveCommentLike(Long commentId, Member member) {
+        memberRepository.findByUsername(member.getUsername()).orElseThrow(
+                () -> new BlogException(BlogExceptionType.MEMBER_NOT_FOUND)
+        );
+        Comment comment = commentRepository.findById(commentId).orElseThrow(
+                () -> new BlogException(BlogExceptionType.BOARD_NOT_FOUND)
+        );
+        Optional<CommentLike> like = commentLikeRepository.findByCommentAndMember(comment, member);
+
+        if(like.isPresent()) {
+            CommentLike commentLike = commentLikeRepository.findByCommentIdAndMemberId(commentId, member.getId());
+            commentLikeRepository.delete(commentLike);
+        }
+        else {
+            CommentLike commentLike = new CommentLike(member, comment.getBoard(), comment);
+            commentLikeRepository.save(commentLike);
+        }
+    }
+
+}
